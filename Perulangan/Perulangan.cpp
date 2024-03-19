14eb84b0256359e25117251d7b1d0cb0c9e07a6f
aivylv <lyvialarasati@gmail.com>
2024-03-19 10:26:52 +0700

    Looping (perulangan) FOR

--- Perulangan/Perulangan.cpp
+++ Perulangan/Perulangan.cpp
@@ -3,5 +3,24 @@ using namespace std;
 
 int main()
 {
-    
+    int i;
+    int arr[5];
+
+    for (i = 60; i > 10; i -= 10) 
+    {
+        cout << 1 << "Selamat Pagi Dunia" << endl;
+    }
+
+    cout << "Nilai i terakhir : " << i << endl;
+
+    for (i = 0; i < 5; i++) 
+    {
+        cout << "Masukkan nilai index ke-" << i << ":";
+        cin >> arr[i];
+    }
+    for (i = 0; i < 5; i++) 
+    {
+        cout << "Data array ke-" << i << ":" << arr[i] << endl;
+    }
+    return 0;
 }
